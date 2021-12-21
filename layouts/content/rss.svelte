1497cffdecfafffbfbcfe3ead54298668b9d1cae
jimafisk <jimafisk@gmail.com>
2021-12-20 21:09:16 -0500

    Initial commit.

---
+++ layouts/content/rss.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let allContent;
+	let allPosts = allContent.filter(content => content.type == "blog");
+
+    let rssFeed = '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"><channel><title>My Site Blog</title><link>https://www.mysite.com/blog</link><description>Awesome blog posts for my site</description>';
+    allPosts.forEach(post => {
+        rssFeed += '<item><title>' + post.fields.title + '</title>';
+        rssFeed += '<link>' + post.path + '</link>';
+        rssFeed += '<description>' + post.fields.body[0] + '</description>';
+        rssFeed += '</item>';
+    });
+    rssFeed += '</channel></rss>';
+
+</script>
+
+<pre style="word-wrap: break-word; white-space: pre-wrap;">
+    {rssFeed}
+</pre>
